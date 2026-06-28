e896671a8224448b81b7e575661ab40ae5cb68bd
tauerlund <tauerlund@gmail.com>
2026-06-28 19:53:27 +0200

    Renaming and minor fixes to Effects settings

--- services/lights/lightController.lua
+++ services/lights/lightController.lua
@@ -90,7 +90,7 @@ function this.onDialogueStarted(e)
     this.smoothedTicks = 0
     this.paused = false
 
-    if this.settings.effectsMode == "scene" then
+    if this.settings.effectsMode == "currentCell" then
         this.buildSceneLights(e.npc)
     else
         this.buildNpcLight(e.npc)
@@ -172,7 +172,7 @@ function this.buildSceneLights(npc)
     end
 
     local origin = npc.position
-    local maxDist = this.settings.effectsSceneDistance
+    local maxDist = this.settings.effectsCellDistance
 
     for ref in cell:iterateReferences(tes3.objectType.light, false) do
         this.tryAddLight(ref, ref.object --[[@as tes3light]], origin, maxDist)
